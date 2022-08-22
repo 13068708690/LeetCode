8d032e6e7ccf6617a638ea4db1ede42e242942be
jackchen <jackchen@futunn.com>
2022-08-22 22:27:24 +0800

    add sum of factorial

---
+++ src/class0/Code01_SumOfFactorial.py
@@ -0,0 +1,22 @@
+"""
+@author: jackchen
+给定整数N，返回1！+ 2！+ ... +N!
+"""
+
+
+def calSumOfFactorial(n):
+    factor = 1
+    factor_sum = 0
+    if n<=0:
+        return factor_sum
+    else:
+        for i in range(1, n+1):
+            factor = factor * i
+            factor_sum = factor_sum+factor
+        return factor_sum
+
+
+if __name__ == '__main__':
+    res = calSumOfFactorial(4)
+    print(res)
+
